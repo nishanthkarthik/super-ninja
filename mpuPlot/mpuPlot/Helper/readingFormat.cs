6ed4483b933e829cf9be710a88581e65532f3ea1
nishanthkarthik <nishanthkarthik@live.com>
2015-02-03 23:04:24 +0530

    Created Viewmodel

---
+++ mpuPlot/mpuPlot/Helper/readingFormat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mpuPlot.Helper
+{
+    class readingFormat
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+    }
+}
--- mpuPlot/mpuPlot/ViewModel/plotViewModel.cs
+++ mpuPlot/mpuPlot/ViewModel/plotViewModel.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Wpf;
+using System.Windows.Forms.DataVisualization.Charting;
+using Newtonsoft.Json;
+using System.IO;
+using mpuPlot.Helper;
 
 namespace mpuPlot.ViewModel
 {
     class plotViewModel
     {
+        public string Title { get; private set; }
+        public IList<DataPoint> xCoord { get; set; }
+        public IList<DataPoint> yCoord { get; set; }
+        public IList<DataPoint> zCoord { get; set; }
+        async public plotViewModel()
+        {
+            this.Title = "mpu";
+            StreamReader _reader = new StreamReader("mpu.json");
+            string readingString = await _reader.ReadToEndAsync();
+            IList<readingFormat> readingStore = await JsonConvert.DeserializeObjectAsync<IList<readingFormat>>(readingString);
+            if (readingStore != null)
+            {
+                xCoord = new IList<DataPoint>() { };
+                yCoord = new IList<DataPoint>() { };
+                zCoord = new IList<DataPoint>() { };
+                for (int i = 0; i < readingStore.Count; i++)
+                {
 
+                }
+            }
+        }
     }
 }
